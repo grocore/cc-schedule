ed4ac5da674dae4bc1a6ddf7d9f9304242e16980
Vladyslav Liebiediev <vladyslav.liebiediev@gmail.com>
2021-07-13 07:46:53 +0300

    Testing MSSQL connection

--- mssql_connect.py
+++ mssql_connect.py
@@ -1,10 +1,13 @@
-import sqlalchemy
+import sqlalchemy as db
 import logging
+import pyodbc
+from sqlalchemy import inspect
+
 
 USERNAME = 'sa'
 PASSWORD = 'passworD0'
 SERVER = 'localhost'
-DATABASE = 'testDB'
+DATABASE = 'oktell_settings'
 DRIVER = 'ODBC+DRIVER+17+for+SQL+Server'
 
 logging.basicConfig(
@@ -21,7 +24,15 @@ engine_stmt = (
     )
 
 try:
-    engine = sqlalchemy.create_engine(engine_stmt, echo=True)
+    drivers = [item for item in pyodbc.drivers()]
+    print(pyodbc.drivers())
+    engine = db.create_engine(engine_stmt, echo=True)
     logging.info('Trying to connect MSSQL')
+    inspector = inspect(engine)
+    print('1')
+    for table_name in inspector.get_table_names():
+        for column in inspector.get_columns(table_name):
+            print("Column: %s" % column['name'])
+
 except Exception as e:
     logging.error('{}'.format(e))
