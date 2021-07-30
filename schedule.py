e9b3668386cf8fed830841ecee814b9a21bacc42
Vladyslav Liebiediev <vladyslav.liebiediev@gmail.com>
2021-07-30 17:06:04 +0300

    Adding main route

--- schedule.py
+++ schedule.py
@@ -8,23 +8,29 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 
-app = Flask(__name__)
 
-engine_stmt = (
+database_uri = (
     'mssql+pyodbc://{}:{}@{}/{}?driver={}'.format(
         USERNAME, PASSWORD, SERVER, DATABASE, DRIVER
         )
     )
 
+app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = engine_stmt
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 oktell_users = db.Table('A_Users', db.metadata, autoload=True, autoload_with=db.engine)
 
+@app.route("/")
+def homepage():
+    return '<h1>Hi</h1>'
+
 @app.route("/schedule/<user_id>", methods=['GET'])
 def user_page(user_id):
     user = db.session.query(oktell_users).filter_by(ID=user_id).first()
-    print(user.ID)
+    user_name = user.Name
+    print(user_name)
     return "<p>Works</p>"
