a6464f603d42f824cd3dcb412a96719b8fb886ef
Vladyslav Liebiediev <vladyslav.liebiediev@gmail.com>
2021-08-03 07:52:54 +0300

    Adding Schedule model

--- schedule.py
+++ schedule.py
@@ -7,6 +7,7 @@ DRIVER = 'ODBC+DRIVER+17+for+SQL+Server'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
+from datetime import date, datetime
 
 
 database_uri = (
@@ -24,6 +25,17 @@ db = SQLAlchemy(app)
 
 oktell_users = db.Table('A_Users', db.metadata, autoload=True, autoload_with=db.engine)
 
+class Schedule(db.Model):
+    __tablename__ = 'A_Schedule'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), nullable=False)
+    start = db.Column(db.DateTime, nullable=False)
+    end = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
 @app.route("/")
 def homepage():
     return '<h1>Hi</h1>'
@@ -32,5 +44,6 @@ def homepage():
 def user_page(user_id):
     user = db.session.query(oktell_users).filter_by(ID=user_id).first()
     user_name = user.Name
-    print(user_name)
+    print(type(user_id))
+    current_time = datetime.now()
     return "<p>Works</p>"
