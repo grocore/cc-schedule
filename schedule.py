c7dbe7e36890e8486c2167957d59c5e4722af243
Vladyslav Liebiediev <vladyslav.liebiediev@gmail.com>
2021-08-18 00:25:36 +0300

    Adding 'add' function

--- schedule.py
+++ schedule.py
@@ -5,7 +5,7 @@ DATABASE = 'oktell_settings'
 DRIVER = 'ODBC+DRIVER+17+for+SQL+Server'
 
 import re
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
@@ -20,6 +20,7 @@ database_uri = (
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "zP0B8YTzRy9vcGMqVEKFFMqoT4kzn7IU"
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -37,8 +38,6 @@ class Schedule(db.Model):
     end = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.Integer)
 
-    def __repr__(self):
-        return '<User %r>' % self.user_id
 
 @app.route("/")
 def homepage():
@@ -50,6 +49,7 @@ def list(user_id):
     print(request.method)
     if user_id == 'test':
         user_id = '15a05eb8-2cd9-4af6-bd93-6960bf50e5ae'
+    session['operator'] = user_id
     if request.method == 'POST':
         if request.form['action'] == 'Отменить':
             print('1')
@@ -75,7 +75,37 @@ def list(user_id):
             print(start)
     user = db.session.query(oktell_users).filter_by(ID=user_id).first()
     records = Schedule.query.order_by(Schedule.start.desc()).all()
-    return render_template('schedule2.html', records=records, user_name=user.Name)
+    return render_template('schedule2.html', records=records, user_name=user.Name, operator=session['operator'])
+
+@app.route("/add/", methods=['POST'])
+def add():
+    print(session['operator'])
+    print(request.method)
+    user_id = session['operator']
+    if request.method == 'POST':
+        if request.form['action'] == 'Отменить':
+            print('1')
+            record_id = request.form.get('record_id')
+            record = Schedule.query.get(record_id)
+            record.status = 3
+            print(request.form.get('action'))
+            print(request.form.get('record_id'))
+            print(type(record_id))
+            db.session.commit()
+        elif request.form['action'] == 'Добавить':
+            print('2')
+            start = datetime.strptime(' '.join([request.form.get('Date'), request.form.get('Start')]), '%Y-%m-%d %H:%M')
+            end = datetime.strptime(' '.join([request.form.get('Date'), request.form.get('End')]), '%Y-%m-%d %H:%M')
+            record = Schedule(user_id=user_id, start=start, end=end, status=1)
+            db.session.add(record)
+            db.session.commit()
+            #end = datetime.strptime(request.form.get('End'), '%H:%M')
+            #print(type(start))
+            #shift_start = datetime.combine(date, start)
+            #print(shift_start)
+            #print(start, end, date)
+            print(start)
+    return redirect(url_for('list', user_id=user_id))
 
 
 @app.route("/schedule/<user_id>", methods=['GET'])
@@ -94,3 +124,6 @@ def user_page(user_id):
     db.session.add(record)
     db.session.commit()
     return "<p>Works</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
